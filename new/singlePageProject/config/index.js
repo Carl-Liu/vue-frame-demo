2068881e9e07005706573a1e35dead2ba8071b8d
DESKTOP-F6FT34E\gerti <gertei201008>
2019-03-26 17:22:55 +0800

    new_打包配置：绝对路径改为相对路径

--- new/singlePageProject/config/index.js
+++ new/singlePageProject/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/demoPost': {
         target: targetUrl,
@@ -52,7 +52,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
